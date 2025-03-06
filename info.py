e888bf31a203bbded6b82b3aef14b4e1080e9553
Devshukla25 <7shuklashivam13@gmail.com>
2025-03-06 09:04:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://vault.pictures/p/cbcaec0548614a6d953b6ffa5d79b64b')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/8d4dad158704cc292c2b6.jpg')).split()
 
 
 # Admins & Users
