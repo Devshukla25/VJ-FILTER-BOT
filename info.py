db23dbf2bdf7aa9ce1d77673fc79e40caa02a974
Devshukla25 <7shuklashivam13@gmail.com>
2025-03-06 08:46:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,7 +11,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'Arpit Shukla')
-API_ID = int(environ.get('API_ID', '- 21566863'))
+API_ID = int(environ.get('API_ID', '21566863'))
 API_HASH = environ.get('API_HASH', '6c301cab6e3488a29eba3e9820d49bfa')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
